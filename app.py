835db02c39a4a418e85b86ba5a6db1b271d708bb
Raphael <r.schnaitl@gmail.com>
2019-06-07 10:12:10 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import cdk
+
+from aws_action_watchdog.aws_action_watchdog_stack import AwsActionWatchdogStack
+
+
+app = cdk.App()
+AwsActionWatchdogStack(app, "aws-action-watchdog-cdk-1")
+
+app.run()
