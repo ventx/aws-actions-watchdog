ea3208407593821fab3f0d78b27a70dedbbf2e61
Raphael <r.schnaitl@gmail.com>
2019-06-17 10:23:09 +0200

    added diff logic

--- aws_action_watchdog/aws_action_watchdog_stack.py
+++ aws_action_watchdog/aws_action_watchdog_stack.py
@@ -5,7 +5,7 @@ class AwsActionWatchdogStack(cdk.Stack):
     def __init__(self, app: cdk.App, id: str, **kwargs) -> None:
         super().__init__(app, id)
 
-        jsonFileBucket = s3.Bucket(self, 'jsonFileBucket', versioned=True)
+        jsonFileBucket = s3.Bucket(self, 'jsonFileBucket', versioned=True, removal_policy=cdk.DeletionPolicy.Delete)
 
         with open("aws_action_watchdog/lambda-handler.js", encoding="utf8") as fp:
             handler_code = fp.read()
@@ -15,7 +15,7 @@ class AwsActionWatchdogStack(cdk.Stack):
         actionWatchdogLambda = lambda_.Function(self, 'watchdog_Lambda2', 
             runtime= lambda_.Runtime.NODE_J_S810,
             code= lambda_.Code.inline(handler_code),
-            handler='index.main')
+            handler='index.handler')
 
         jsonFileBucket.grant_read_write(actionWatchdogLambda)
         
--- aws_action_watchdog/lambda-handler.js
+++ aws_action_watchdog/lambda-handler.js
@@ -1,39 +1,68 @@
+const https = require("https"),
+  vm = require("vm"),
+  AWS = require("aws-sdk");
 
-const https = require('https'),
-    vm = require('vm'),
-    AWS = require('aws-sdk');
-
-let url = "https://awsiamconsole.s3.amazonaws.com/iam/assets/js/bundles/policies.js"
+let url =
+  "https://awsiamconsole.s3.amazonaws.com/iam/assets/js/bundles/policies.js";
 
 exports.handler = async function(event) {
   const promise = new Promise(function(resolve, reject) {
     let s3 = new AWS.S3();
-    s3.putObject()
-    https.get(url, (res) => {
-        let data = ""
-        res.on('data', (chunk) => {
-            data += chunk
-        })
-        res.on('end', () => {
-          let defineTypes = 'let app = {};'  
-          defineTypes += 'let EnvInfo = {};'
-          defineTypes += 'let _ = { has: function () { return false; }, extend: function () { }};'
-          defineTypes += 'let window = { EnvInfo: {} };'
-          data = defineTypes + data + ";app;"
-          let result = vm.runInThisContext(data, url)
-          let json = JSON.stringify(result)
-          params = {Bucket: "{{S3BucketName}}", Key: "services.json", Body: json  }
-          s3.putObject(params, function(err,data) {
-            if(err) {
-              reject(err)
+    s3.putObject();
+    https
+      .get(url, res => {
+        let data = "";
+        res.on("data", chunk => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          let defineTypes = "let app = {};";
+          defineTypes += "let EnvInfo = {};";
+          defineTypes +=
+            "let _ = { has: function () { return false; }, extend: function () { }};";
+          defineTypes += "let window = { EnvInfo: {} };";
+          data = defineTypes + data + ";app;";
+          let result = vm.runInThisContext(data, url);
+          let listOfListOfActions = Object.values(
+            result.PolicyEditorConfig.serviceMap
+          ).map(x => x.Actions.map(a => x.StringPrefix + ":" + a));
+          let actions = listOfListOfActions.reduce(function(a, b) {
+            return a.concat(b);
+          });
+
+          let getParams = {
+            Bucket: "{{S3BucketName}}",
+            Key: "services.json"
+          };
+
+          s3.getObject(getParams, function(err, data) {
+            if (err) {
+              console.log(err, err.stack);
             } else {
-              resolve(res.statusCode)
+              var oldActions = JSON.parse(data);
+
+              var newActions = actions.filter(x => !oldActions.includes(x))
+              console.log(newActions)
+
+              let params = {
+                Bucket: "{{S3BucketName}}",
+                Key: "services.json",
+                Body: JSON.stringify(actions)
+              };
+              s3.putObject(params, function(err, data) {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve(res.statusCode);
+                }
+              });
             }
-          })
-        })
-      }).on('error', (e) => {
-        reject(Error(e))
+          });
+        });
       })
-    })
-  return promise
-}
+      .on("error", e => {
+        reject(Error(e));
+      });
+  });
+  return promise;
+};
