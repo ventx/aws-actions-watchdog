bed71a4d533e5a20e557bc13a667d32ded9ec641
Raphael <r.schnaitl@gmail.com>
2019-06-17 16:29:31 +0200

    added a cloudwatch alarm

--- aws_action_watchdog/aws_action_watchdog_stack.py
+++ aws_action_watchdog/aws_action_watchdog_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import cdk, aws_lambda as lambda_, aws_iam as iam, aws_s3 as s3
+from aws_cdk import cdk, aws_lambda as lambda_, aws_iam as iam, aws_s3 as s3, aws_sns as sns, aws_cloudwatch as cloudwatch
 
 class AwsActionWatchdogStack(cdk.Stack):
 
@@ -6,16 +6,30 @@ class AwsActionWatchdogStack(cdk.Stack):
         super().__init__(app, id)
 
         jsonFileBucket = s3.Bucket(self, 'jsonFileBucket', versioned=True, removal_policy=cdk.DeletionPolicy.Delete)
+        newActionTopic = sns.Topic(self, 'newActions')
+        lambdaErrorSnsTopic = sns.Topic(self, 'lambdaError')
 
         with open("aws_action_watchdog/lambda-handler.js", encoding="utf8") as fp:
             handler_code = fp.read()
 
         handler_code = handler_code.replace('{{S3BucketName}}', jsonFileBucket.bucket_name)
+        handler_code = handler_code.replace('{{SnsTopicArn}}', newActionTopic.topic_arn)
 
         actionWatchdogLambda = lambda_.Function(self, 'watchdog_Lambda2', 
             runtime= lambda_.Runtime.NODE_J_S810,
             code= lambda_.Code.inline(handler_code),
             handler='index.handler')
 
+        lambdaErrorAlarm = cloudwatch.Alarm(self, 'lambdaErrorAlarm', 
+            evaluation_periods=5, 
+            threshold=0, 
+            comparison_operator=cloudwatch.ComparisonOperator.GreaterThanThreshold,
+            treat_missing_data=cloudwatch.TreatMissingData.Breaching,
+            alarm_name='Lambda Failed',
+            metric=cloudwatch.Metric(metric_name= 'Errors', namespace='AWS/Lambda',dimensions= {
+                'FunctionName': actionWatchdogLambda.function_name
+            }))
+        
+        newActionTopic.grant_publish(actionWatchdogLambda)
         jsonFileBucket.grant_read_write(actionWatchdogLambda)
         
--- aws_action_watchdog/lambda-handler.js
+++ aws_action_watchdog/lambda-handler.js
@@ -2,60 +2,44 @@ const https = require("https"),
   vm = require("vm"),
   AWS = require("aws-sdk");
 
-let url =
-  "https://awsiamconsole.s3.amazonaws.com/iam/assets/js/bundles/policies.js";
+let url = "https://awsiamconsole.s3.amazonaws.com/iam/assets/js/bundles/policies.js";
+let bucketName = "{{S3BucketName}}"
+let snsTopicArn = "{{SnsTopicArn}}"
 
-exports.handler = async function(event) {
-  const promise = new Promise(function(resolve, reject) {
+exports.handler = async event => {
+  return main()
+};
+
+function main() {
+    return new Promise(function(resolve, reject) {
     let s3 = new AWS.S3();
-    s3.putObject();
-    https
-      .get(url, res => {
+    let sns = new AWS.SNS();
+
+    https.get(url, res => {
         let data = "";
         res.on("data", chunk => {
           data += chunk;
         });
         res.on("end", () => {
-          let defineTypes = "let app = {};";
-          defineTypes += "let EnvInfo = {};";
-          defineTypes +=
-            "let _ = { has: function () { return false; }, extend: function () { }};";
-          defineTypes += "let window = { EnvInfo: {} };";
-          data = defineTypes + data + ";app;";
-          let result = vm.runInThisContext(data, url);
-          let listOfListOfActions = Object.values(
-            result.PolicyEditorConfig.serviceMap
-          ).map(x => x.Actions.map(a => x.StringPrefix + ":" + a));
-          let actions = listOfListOfActions.reduce(function(a, b) {
-            return a.concat(b);
-          });
-
+          let actions = getActions(data)
           let getParams = {
-            Bucket: "{{S3BucketName}}",
+            Bucket: bucketName,
             Key: "services.json"
           };
 
           s3.getObject(getParams, function(err, data) {
             if (err) {
-              console.log(err, err.stack);
+              if (err.errorType == "NoSuchKey") {
+                saveValues(s3, actions, reject, resolve);
+              } else {
+                reject(err);
+              }
             } else {
-              var oldActions = JSON.parse(data);
-
-              var newActions = actions.filter(x => !oldActions.includes(x))
-              console.log(newActions)
-
-              let params = {
-                Bucket: "{{S3BucketName}}",
-                Key: "services.json",
-                Body: JSON.stringify(actions)
-              };
-              s3.putObject(params, function(err, data) {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(res.statusCode);
-                }
-              });
+              var oldActions = JSON.parse(data.Body.toString('utf-8'));
+              var newActions = actions.filter(x => !oldActions.includes(x));
+              writeToSnsTopic(sns,'New Actions', JSON.stringify(newActions, null, 2))
+              console.log(newActions);
+              saveValues(s3, actions, reject, resolve);
             }
           });
         });
@@ -64,5 +48,46 @@ exports.handler = async function(event) {
         reject(Error(e));
       });
   });
-  return promise;
-};
+}
+
+
+function writeToSnsTopic(sns, title, message) {
+  var params  ={ 
+    Message: message,
+    Subject: title,
+    TopicArn: snsTopicArn
+  }
+
+  return sns.publish(params).promise()
+}
+
+function getActions(data) {
+  let defineTypes = "let app = {};";
+  defineTypes += "let EnvInfo = {};";
+  defineTypes +=
+    "let _ = { has: function () { return false; }, extend: function () { }};";
+  defineTypes += "let window = { EnvInfo: {} };";
+  data = defineTypes + data + ";app;";
+  let result = vm.runInThisContext(data, url);
+  let listOfListOfActions = Object.values(
+    result.PolicyEditorConfig.serviceMap
+  ).map(x => x.Actions.map(a => x.StringPrefix + ":" + a));
+  return listOfListOfActions.reduce(function(a, b) {
+    return a.concat(b);
+  });
+}
+
+function saveValues(s3, actions, reject, resolve) {
+  let params = {
+    Bucket: "{{S3BucketName}}",
+    Key: "services.json",
+    Body: JSON.stringify(actions)
+  };
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(200);
+    }
+  });
+}
